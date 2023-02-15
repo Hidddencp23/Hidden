fdc8956d11ccd87e05facb0343ccf33ef5516940
PraneethSomisetty1 <psomiset@calpoly.edu>
2023-02-15 11:24:51 -0800

    changed to flatlist, updated some styling

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,53 +1,65 @@
+import React from "react";
+import { useState } from "react";
+import SegmentedControl from "@react-native-segmented-control/segmented-control";
 
-import React from 'react';
-import { useState } from 'react';
-import BottomNavBar from '../components/BottomNavBar';
-import { Text, StyleSheet, View, Image, TouchableOpacity, FlatList, SafeAreaView, ScrollView } from 'react-native';
-import { collection, where, onSnapshot, orderBy, query } from 'firebase/firestore';
-import useAuth from '../hooks/useAuth';
-import ProfileCard from '../components/ProfileCard';
+import BottomNavBar from "../components/BottomNavBar";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import {
+  collection,
+  where,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import useAuth from "../hooks/useAuth";
+import ProfileCard from "../components/ProfileCard";
 
-import Icon from 'react-native-vector-icons/AntDesign';
+import Icon from "react-native-vector-icons/AntDesign";
 //import { Icon } from 'react-native-vector-icons';
 
 // placeholder image for now
-import deleteme from '../../assets/deleteme.png';
+import deleteme from "../../assets/deleteme.png";
 
 // need to connect db
 //import { db } from '../hooks/firebase';
 
-
-
-const ProfileScreen = ({ navigation }) => { 
+const ProfileScreen = ({ navigation }) => {
   const { user, logout } = useAuth();
   const [data, setData] = React.useState([]);
 
-
-
   // this info comes from firbase, placeholders for the moment
-  const [firstName, setFirstName] = useState('John');
-  const [lastName, setLastName] = useState('Doe');
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Doe");
   const [friends, setFriends] = useState(0);
   const [online, setOnline] = useState(1); // flag for indicator
 
   // state for liked / my trips toggle
-  const [displayTrips, setdisplayTrips] = useState('');
-
+  const [displayTrips, setdisplayTrips] = useState("My Trips");
 
   // placeholder trips to see if it works
   const extrip1 = {
-    user: 'Praneeth',
-    title: 'Barcelona Trip'
+    key: "1",
+    user: "Praneeth",
+    title: "Barcelona Trip",
   };
 
   const extrip2 = {
-    user: 'Arden',
-    title: 'Rome Trip'
+    key: "2",
+    user: "Arden",
+    title: "Rome Trip",
   };
 
-  const exLikedTrips = [ extrip1, extrip2, extrip1, extrip1, extrip1 ]
-  const exMyTrips = [ extrip2, extrip2, extrip2, extrip2, extrip2, extrip2 ]
-
+  const exLikedTrips = [extrip1, extrip2];
+  const exMyTrips = [extrip2, extrip1];
 
   // db is not connected yet
   /*
@@ -61,143 +73,65 @@ const ProfileScreen = ({ navigation }) => {
     )
   ) 
   */
-
-
+  const TripView = ({ trip }) => (
+    <View
+      style={{
+        borderBottomColor: "black",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        marginLeft: "5%",
+        marginRight: "5%",
+      }}
+    >
+      <TouchableOpacity style={styles.myTripTab}>
+        <View style={styles.horizButtons}>
+          <View style={styles.vertButtons}>
+            <Text style={styles.myTripsTitle}>{trip.title}</Text>
+            <Text style={styles.myTripsUser}>{"By: " + trip.user}</Text>
+          </View>
+          <Icon name="right" size={20} style={styles.arrow} />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
-    <SafeAreaView>
-      <ScrollView>
-        
+    <SafeAreaView style={{ height: "100%" }}>
       <View style={styles.profTop}>
-
-        <View style={styles.photoAlign}>
-          <Image source={deleteme} alt="Avatar" style={styles.photoURL}/>
-        </View>
-
-        <View style={styles.upTextAlign}>
-          <View style={styles.setHorizontal}>
-            <Text style={styles.profileTitle}>{firstName} {lastName} </Text>
-
-            { online === 1 ? 
-              <>
-                <View style={styles.onlineStatusCircle}></View>
-              </>
-              :
-              <>
-                <View style={styles.offlineStatusCircle}></View>
-              </>
-            }
-
-          </View>
-          <Text style={styles.profileSubTitle}>{friends} Friends</Text>
-        </View>
+        <View style={styles.circle} />
+        <Image source={deleteme} alt="Avatar" style={styles.photoURL} />
+        <Text style={styles.profileTitle}>
+          {firstName} {lastName}{" "}
+        </Text>
+        <Text style={styles.profileSubTitle}>{friends} Friends</Text>
       </View>
-
-
-      <View style={styles.horizButtons}>
-        <TouchableOpacity style={styles.leftToggleButton} onPress={() => {setdisplayTrips('liked')}}>
-            <Text style={styles.buttonText}>Liked Trips</Text>
-        </TouchableOpacity>
-        <View
-          style={{
-            borderRightColor: 'black',
-            borderRightWidth: StyleSheet.hairlineWidth,
-            marginTop: '5%',
-            marginBottom: '5%'
-          }}
-        />
-        <TouchableOpacity style={styles.rightToggleButton} onPress={() => {setdisplayTrips('mine')}}>
-            <Text style={styles.buttonText}>My Trips</Text>
-        </TouchableOpacity>
-      </View>
-      
-
+      <SegmentedControl
+      style={styles.toggleButton}
+        values={["Liked Trips", "My Trips"]}
+        selectedIndex={displayTrips}
+        onValueChange={(value) => {
+          setdisplayTrips(value);
+        }}
+      />
       <View>
-
-      { displayTrips === 'mine' ? 
-
-        <>
-          {exMyTrips.map((trip) => {
-
-                return (
-                  <>
-                    <TouchableOpacity style={styles.myTripTab}>
-
-                      <View style={styles.horizButtons}>
-                        <View style={styles.vertButtons}>
-                          <Text style={styles.myTripsTitle}>{trip.title}</Text>
-                          <Text style={styles.myTripsUser}>{'By: ' + trip.user}</Text>
-                        </View>
-                      <Icon name="right" size={20} style={styles.arrow}/>
-                      </View>
-                    </TouchableOpacity>
-                  </>
-                )
-          })}
-        </> 
-      
-        : null
-    
-      }
-
-
-      { displayTrips === 'liked' ? 
-
-        <>
-
-          {exLikedTrips.map((trip) => {
-              return (
-                <>
-                  <View
-                    style={{
-                      borderBottomColor: 'black',
-                      borderBottomWidth: StyleSheet.hairlineWidth,
-                      marginLeft: '5%',
-                      marginRight: '5%'
-                    }}
-                  />
-                  <TouchableOpacity style={styles.likedTripTab}>
-                      <View style={styles.horizButtons}>
-                        <View style={styles.vertButtons}>
-                          <Text style={styles.myTripsTitle}>{trip.title}</Text>
-                          <Text style={styles.myTripsUser}>{'By: ' + trip.user}</Text>
-                        </View>
-                      <Icon name="right" size={20} style={styles.arrow}/>
-                      </View>
-                  </TouchableOpacity>
-                </>
-              )
-
-          })}
-
-          <View
-            style={{
-              borderBottomColor: 'black',
-              borderBottomWidth: StyleSheet.hairlineWidth,
-              marginLeft: '5%',
-              marginRight: '5%'
-            }}
+        {displayTrips === "My Trips" ? (
+          <FlatList
+            data={exMyTrips}
+            renderItem={({ item }) => <TripView trip={item} />}
           />
-        </>
+        ) : null}
 
-        : null
-    }
-
-       </View>
-
-
-
-
-      
-    </ScrollView>
+        {displayTrips === "Liked Trips" ? (
+          <FlatList
+            data={exLikedTrips}
+            renderItem={({ item }) => <TripView trip={item} />}
+          />
+        ) : null}
+      </View>
     </SafeAreaView>
-  )
+  );
 };
 
-
-
 const styles = StyleSheet.create({
-
   /*
   original CSS - keeping as reference in case anything breaks
 
@@ -433,237 +367,212 @@ const styles = StyleSheet.create({
   }
   */
 
-
   profTop: {
-    backgroundColor: '#77C3EC',
-    height: 300,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: "#77C3EC",
+    height: "30%",
     borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30
+    borderBottomRightRadius: 30,
   },
-
-  
   setHorizontal: {
-    width: '100%',
+    width: "100%",
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: "column",
   },
-
-
   photoURL: {
     height: 110,
     width: 110,
     borderRadius: 100,
-    borderStyle: 'solid',
-    borderWidth: 5
+    borderStyle: "solid",
+    borderWidth: 5,
   },
   photoAlign: {
-    marginTop: '20%',
-    marginLeft: '10%',
-    textAlign: 'left',
+    marginTop: "20%",
+    marginLeft: "10%",
+    textAlign: "left",
   },
 
   upTextAlign: {
-    marginTop: '25%',
-    marginLeft: '10%',
-    position: 'absolute',
-    marginLeft: '50%',
-    marginTop: '25%',
-    marginRight: '15%'
+    marginTop: "25%",
+    marginLeft: "10%",
+    position: "absolute",
+    marginLeft: "50%",
+    marginTop: "25%",
+    marginRight: "15%",
   },
-
 
   profileTitle: {
     fontSize: 30,
     fontWeight: "bold",
-    color: 'white'
+    color: "white",
   },
   profileSubTitle: {
     fontSize: 20,
     fontWeight: "bold",
-    color: 'white'
+    color: "white",
   },
 
   onlineStatusCircle: {
     width: 10,
     height: 10,
     borderRadius: 100,
-    backgroundColor: 'white',
-    borderColor: 'white',
-    marginLeft: '110%',
-    marginTop: '5%',
-    position: 'absolute'
+    backgroundColor: "white",
+    borderColor: "white",
+    marginLeft: "110%",
+    marginTop: "5%",
+    position: "absolute",
   },
 
   offlineStatusCircle: {
     width: 10,
     height: 10,
-    borderRadius: '50%',
-    backgroundColor: 'grey',
-    borderColor: 'white',
-    marginLeft: '110%',
-    marginTop: '5%',
-    position: 'absolute'
+    borderRadius: "50%",
+    backgroundColor: "grey",
+    borderColor: "white",
+    marginLeft: "110%",
+    marginTop: "5%",
+    position: "absolute",
   },
-
 
   setHorizontalButtons: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    borderRadius: 20
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    borderRadius: 20,
   },
 
-  leftToggleButton: {
-    backgroundColor: '#D3D3D3',
-    borderBottomLeftRadius: 20,
-    borderTopLeftRadius: 20,
-    marginLeft: '10%',
-    marginTop: '5%',
-    width: '40%',
-    marginBottom: '5%'
-  },
-  rightToggleButton: {
-    backgroundColor: '#D3D3D3',
-    borderBottomRightRadius: 20,
-    borderTopRightRadius: 20,
-    marginTop: '5%',
-    marginRight: '10%',
-    width: '40%',
-    marginBottom: '5%'
-  },
+  toggleButton: {
+    tintColor:"#83C3FF",
 
+    backgroundColor: "#FFFFFF",
+    height: "7%",
+    marginLeft: "10%",
+    marginTop: "5%",
+    width: "80%",
+    marginBottom: "5%",
+  },
 
   buttonText: {
-    marginTop: '5%',
-    marginBottom: '5%',
-    color: 'black',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    marginTop: "5%",
+    marginBottom: "5%",
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "center",
     fontSize: 16,
   },
 
   horizButtons: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
 
   vertButtons: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
   },
 
   myTripsTitle: {
-    marginLeft: '5%',
-    color: 'black',
-    fontWeight: 'bold',
-    textAlign: 'left',
+    marginLeft: "5%",
+    color: "black",
+    fontWeight: "bold",
+    textAlign: "left",
     fontSize: 16,
-    marginTop: '3%',
-    marginLeft: '5%'
+    marginTop: "3%",
+    marginLeft: "5%",
   },
 
   myTripsUser: {
-    marginLeft: '5%',
-    color: 'black',
-    textAlign: 'left',
+    marginLeft: "5%",
+    color: "black",
+    textAlign: "left",
     fontSize: 14,
-    marginBottom: '5%'
-
+    marginBottom: "5%",
   },
 
   myTripTab: {
-    backgroundColor: '#BEBEBE',
-    marginTop: '5%',
-    marginLeft: '10%',
-    marginRight: '10%',
+    backgroundColor: "#BEBEBE",
+    marginTop: "5%",
+    marginLeft: "10%",
+    marginRight: "10%",
     height: 75,
-    borderRadius: 20
+    borderRadius: 20,
   },
 
   likedTripTab: {
-    backgroundColor: 'transparent',
-    marginTop: '5%',
-    marginLeft: '10%',
-    marginRight: '10%',
+    backgroundColor: "transparent",
+    marginTop: "5%",
+    marginLeft: "10%",
+    marginRight: "10%",
     height: 75,
     borderRadius: 20,
-
   },
 
   line: {
     borderRadius: 20,
-    borderColor: 'black',
-    borderWidth: 20
+    borderColor: "black",
+    borderWidth: 20,
   },
 
   arrow: {
-    marginLeft: '85%',
-    float: 'left',
-    marginTop: '7.5%',
-    position: 'absolute'
+    marginLeft: "85%",
+    float: "left",
+    marginTop: "7.5%",
+    position: "absolute",
   },
-
-
-  
-
-
-
-
 
   profileScreen: {
-    paddingBottom: 75
+    paddingBottom: 75,
   },
   profile: {
-    height: '100%',
-    alignItems: 'center',
-    paddingTop: '10%'
+    height: "100%",
+    alignItems: "center",
+    paddingTop: "10%",
   },
   profileName: {
     fontSize: 30,
-    marginTop: '5%'
+    marginTop: "5%",
   },
   profileText: {
     fontSize: 18,
-    marginTop: '2%'
+    marginTop: "2%",
   },
-  
+
   profButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginTop: 30,
-    marginBottom: 30
+    marginBottom: 30,
   },
-  
+
   logoutButton: {
-    backgroundColor: '#0984e3',
+    backgroundColor: "#0984e3",
     paddingVertical: 10,
     paddingHorizontal: 40,
-    borderRadius: 20
+    borderRadius: 20,
   },
   logoutText: {
-    backgroundColor: '#0984e3',
+    backgroundColor: "#0984e3",
     paddingVertical: 10,
     paddingHorizontal: 40,
-    borderRadius: 20
+    borderRadius: 20,
   },
   editButton: {
-    backgroundColor: '#0984e3',
+    backgroundColor: "#0984e3",
     paddingHorizontal: 30,
     paddingVertical: 10,
     marginRight: 30,
-    borderRadius: 20
+    borderRadius: 20,
   },
-  
+
   profTextAlign: {
-    display: 'table-cell',
-    textAlignVertical: 'middle',
+    display: "table-cell",
+    textAlignVertical: "middle",
     fontSize: 30,
     fontWeight: "bold",
-    color: 'white'
-  }
+    color: "white",
+  },
 });
 
-
-
 export default ProfileScreen;
-
-
