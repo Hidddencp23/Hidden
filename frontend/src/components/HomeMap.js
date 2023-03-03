f29626b3e9b234a58fdf3a5de60734475fa969eb
Arden <ardenozdere@gmail.com>
2023-03-03 15:42:54 -0800

    map markers

--- frontend/src/components/HomeMap.js
+++ frontend/src/components/HomeMap.js
@@ -1,63 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { View } from 'react-native';
-import * as Location from 'expo-location';
-
+import React, { useEffect, useState } from "react";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { View } from "react-native";
+import * as Location from "expo-location";
 
 const HomeMap = () => {
-    const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [markers, setMarkers] = useState([]);
 
-    const [position, setPosition] = useState({
-        latitude: 10,
-        longitude: 10,
-        latitudeDelta: 0.001,
-        longitudeDelta: 0.001,
-    });
-
-    //   useEffect(() => {
-    //     Geolocation.getCurrentPosition((pos) => {
-    //       const crd = pos.coords;
-    //       setPosition({
-    //         latitude: crd.latitude,
-    //         longitude: crd.longitude,
-    //         latitudeDelta: 0.0421,
-    //         longitudeDelta: 0.0421,
-    //       });
-    //     }).catch((err) => {
-    //       console.log(err);
-    //     });
-    //   }, []);
-
-    useEffect(() => {
-        (async () => {
-
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
-
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-            setPosition({
-                latitude: location.coords.latitude,
-                longitude: location.coords.longitude,
-                latitudeDelta: 0.0421,
-                longitudeDelta: 0.0421,
-            });
-            console.log(location);
-        })();
-    }, []);
+  exMarkers = [{
+    latlng: {
+        latitude: 33.672939,
+        longitude: -117.962395
+    },
+    title: "Example",
+    description: "test desc"
+  }]
 
 
-    return (
-        <MapView provider={PROVIDER_GOOGLE}
-            style={{ flex: 1 }}
-            region={position}
-            showsUserLocation={true}
-            followsUserLocation={true} />
-    )
+  const [position, setPosition] = useState({
+    latitude: 10,
+    longitude: 10,
+    latitudeDelta: 0.001,
+    longitudeDelta: 0.001,
+  });
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setPosition({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0421,
+        longitudeDelta: 0.0421,
+      });
+      console.log(location);
+    })();
+  }, []);
+
+  return (
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={{ flex: 1 }}
+      region={position}
+      showsUserLocation={true}
+      followsUserLocation={true}
+    >
+      {exMarkers.map((marker, index) => (
+        <Marker
+          key={index}
+          coordinate={marker.latlng}
+          title={marker.title}
+          description={marker.description}
+          onPress={() => console.log(marker.title)}
+        />
+      ))}
+    </MapView>
+  );
 };
-
 
 export default HomeMap;
