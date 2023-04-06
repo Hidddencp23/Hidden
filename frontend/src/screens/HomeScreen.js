7df5a9873b300deb4662ff5777c861abb0a8ee5d
PraneethSomisetty1 <psomiset@calpoly.edu>
2023-04-06 13:18:28 -0700

    Revert "Merge pull request #42 from Hidddencp23/Messaging_Screens"
    This reverts commit ae270302a844f7be442e9e7df825d67ba66f1774, reversing
    changes made to 1f13bb42372ac4b68f927b7cdb4d7c9bcf36d233.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -103,9 +103,6 @@ const HomeScreen = ({ navigation }) => {
 
     return (
         <KeyboardAvoidingView behavior="position" style={styles.container} keyboardVerticalOffset={-190}>
-        
-
-
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <SafeAreaView style={styles.homeScreen} >
                 <HomeMap style={styles.map} hiddenLocations={locations} ></HomeMap>
@@ -131,11 +128,6 @@ const HomeScreen = ({ navigation }) => {
                 </TouchableOpacity>
             </SafeAreaView>
             </TouchableWithoutFeedback>
-
-
-    
-
-
     </KeyboardAvoidingView>
     )
 }
