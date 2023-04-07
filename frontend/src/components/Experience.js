0407b296125558beb67627fb1cbdd309f828e3c5
Carly Scheuerman <90992086+carlysch@users.noreply.github.com>
2023-04-07 13:33:44 -0700

    get user info

--- frontend/src/components/Experience.js
+++ frontend/src/components/Experience.js
@@ -1,17 +1,33 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { SafeAreaView, View, Text, StyleSheet, FlatList, TouchableWithoutFeedback, TouchableOpacity, ScrollView } from 'react-native';
-import { db } from '../hooks/firebase';
+import { SafeAreaView, Image, View, Text, StyleSheet, FlatList, TouchableWithoutFeedback, TouchableOpacity, ScrollView } from 'react-native';
+import { where, getDoc, addDoc, onSnapshot, orderBy, query, serverTimestamp, doc } from 'firebase/firestore'
+import { db } from "../hooks/firebase";
 
 
 const Experience = ({ experience }) => {
-    console.log("experience")
-
-    console.log(experience)
-
-
-    return (
+    const [poster, setPoster] = useState("");
+    const getUserNameFromUid = async (uid) => {
+        const docRef = doc(db, "Users", uid);
+        try {
+            const docSnap = await getDoc(docRef);
+            if(docSnap.exists()) {
+                setPoster(docSnap.data());
+            } else {
+                console.log("Document does not exist")
+            }
+        
+        } catch(error) {
+            console.log(error)
+        }
+    }
+    useEffect(() => {
+        getUserNameFromUid(experience.userId).then(() => {
+        }).catch(console.error);
+    }, [])
+        return (
         <SafeAreaView>
-            <Text>{experience.userName}</Text>
+            <Text>{poster["username"]}</Text>
+            <Image source={{ uri: experience.image }} style={styles.locImg} />
             <Text>{experience.description}</Text>
             <Text>{experience.rating}</Text>
             <ScrollView>
@@ -21,5 +37,19 @@ const Experience = ({ experience }) => {
     )
 }
 
-
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height: '100%',
+        backgroundColor: 'white',
+      },
+      locImg: {
+        width: "90%",
+        height: "25%",
+        borderRadius: 15,
+    },
+    });
 export default Experience;
