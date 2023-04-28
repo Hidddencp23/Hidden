c7e5a744c17a2a4e47b28e1855226eaa92306a3b
Carly Scheuerman <90992086+carlysch@users.noreply.github.com>
2023-04-27 22:05:35 -0700

    dont merge

--- frontend/src/screens/EditProfileScreen.js
+++ frontend/src/screens/EditProfileScreen.js
@@ -1,7 +1,4 @@
-import React, { useState } from 'react';
-import UploadImageAndroid from '../components/UploadImageAndroid';
-import { db } from '../hooks/firebase';
-import useAuth from '../hooks/useAuth'
+import React, { Fragment, Component, useState } from 'react';
 import {
   StyleSheet,
   SafeAreaView,
@@ -14,34 +11,44 @@ import {
   TouchableWithoutFeedback,
   Keyboard
 } from "react-native";
-import { setDoc, doc, updateDoc } from "firebase/firestore";
 import Icon from "react-native-vector-icons/AntDesign";
-
+import { MaterialIcons } from '@expo/vector-icons';
+import { collection, serverTimestamp, addDoc} from "firebase/firestore";
+import { db } from '../hooks/firebase';
+import useAuth from '../hooks/useAuth'
+import { useRoute } from '@react-navigation/native'
 import * as ImagePicker from 'expo-image-picker';
 
 import * as FileSystem from 'expo-file-system';
 
-
+// source: https://www.atomlab.dev/tutorials/react-native-star-rating
 
 const EditProfileScreen = ({ navigation }) => {
-  const [changeName, setChangeName] = useState("");
-  const { user, userInfo, logout } = useAuth();
-  const [image, setImage] = useState(null);
-  const handleChangeName = (text) => {
-    setChangeName(text);
+  const { user, userInfo } = useAuth();
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const handleDescriptionChange = (text) => {
+    setDescription(text);
   };
 
-  const handleSubmit = async () => {
-    try {
-      updateDoc(doc(db, "Users", user.uid), {
-        name: changeName,
-        profilePic: image,
-      });
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  }
+  const [name, setName] = useState("");
+  const handleNameChange = (text) => {
+    setName(text);
+  };
+  const [address, setAddress] = useState("");
+  const handleAddressChange = (text) => {
+    setAddress(text);
+  };
+  const [lat, setLat] = useState(0);
+  const handleLatChange = (text) => {
+    setLat(Number(text));
+  };
+  const [long, setLong] = useState(0);
+  const handleLongChange = (text) => {
+    setLong(Number(text));
+  };
 
+  
   const addImage = async () => {
     let _image = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -52,74 +59,176 @@ const EditProfileScreen = ({ navigation }) => {
     if (!_image.canceled) {
       setImage(_image.assets[0]['uri']);
 
-      const base64 = await FileSystem.readAsStringAsync(_image.assets[0]['uri'], { encoding: 'base64' });
+      // const base64 = await FileSystem.readAsStringAsync(_image.assets[0]['uri'], { encoding: 'base64' });
       
       // this is the base64 string of the uploaded image
       // could pass in a setter to get this value to the form
       // (or just pull out the function to the AddExperienceScreen)
       //setImageString(base64);
+
     }
   };
+  
 
+  const handleSubmit = async () => {
+    try {
+      await addPost(name, address, image, description, long, lat);
+    } catch (error) {
+      console.error("Unable to post", error);
+    }
+    console.log("submitted")
+  }
+  const addPost = async (name, address, image, description, long, lat) => {
+    try {
+      addDoc(collection(db, "HiddenLocations"), {
+            name: name,
+            address: address,
+            description: description,
+            image: image,
+            coordinates: {latitude: +lat, longitude: +long},
+            category: [],
+            experiences: [],
+            Liked: [],
+          });
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
+  }
   return (
-      <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={-170}>
+    <KeyboardAvoidingView behavior="position">
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <SafeAreaView style={{ 
-          height: Dimensions.get('window').height,
-          alignItems: 'center'
-        }}>
-          <TouchableOpacity style={styles.circle} onPress={addImage}>
-            <Icon name='cloudupload' size={50} color={"#83C3FF"}/>
+          <View style={styles.body}>
+          <TouchableOpacity style={styles.box} onPress={addImage}>
+            <Icon name='cloudupload' size={20} color={"#83C3FF"}/>
+            <Text>Upload Photo/Video</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.box}>
+            <TouchableOpacity style={styles.box}>
               <TextInput
                       style={styles.textInput}
-                      value={changeName}
-                      onChangeText={handleChangeName}
+                      value={name}
+                      onChangeText={handleNameChange}
+                      maxLength={300}
                       multiline={true}
-                      placeholder="Change Name"
+                      placeholder="Name"
                       placeholderTextColor="#8e8e8e" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.save} onPress={handleSubmit}>
-            <Text style={styles.postText}>Save</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.save} onPress={logout}>
-            <Text style={styles.postText}>Logout</Text>
-          </TouchableOpacity>
-          </SafeAreaView>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.box}>
+              <TextInput
+                      style={styles.textInput}
+                      value={address}
+                      onChangeText={handleAddressChange}
+                      maxLength={300}
+                      multiline={true}
+                      placeholder="Address"
+                      placeholderTextColor="#8e8e8e" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.box}>
+              <TextInput
+                      style={styles.textInput}
+                      value={description}
+                      onChangeText={handleDescriptionChange}
+                      maxLength={300}
+                      multiline={true}
+                      placeholder="Description"
+                      placeholderTextColor="#8e8e8e" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.box}>
+              <TextInput
+                      style={styles.textInput}
+                      value={lat}
+                      onChangeText={handleLatChange}
+                      maxLength={300}
+                      multiline={true}
+                      numeric
+                      placeholder="Latitude"
+                      placeholderTextColor="#8e8e8e" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.box}>
+              <TextInput
+                      style={styles.textInput}
+                      value={long}
+                      onChangeText={handleLongChange}
+                      maxLength={300}
+                      multiline={true}
+                      numeric
+                      placeholder="Longitude"
+                      placeholderTextColor="#8e8e8e" />
+            </TouchableOpacity>
+           
+            <TouchableOpacity style={styles.post} onPress={handleSubmit}>
+              <Text style={styles.postText}>POST</Text>
+            </TouchableOpacity>
+          </View>
      </TouchableWithoutFeedback>
-      </KeyboardAvoidingView>
+    </KeyboardAvoidingView>
   )
+
 };
 
 const styles = StyleSheet.create({
-
+  text: {
+    fontSize: 30,
+    weight: 'bold',
+    paddingLeft: '12%'
+  },
+  text1: {
+    fontSize: 15,
+    weight: 500,
+    paddingLeft: "10%"
+  },
+  rating: {
+    width: "90%",
+    paddingVertical: "5%"
+    
+  },
   textInput: {
     paddingHorizontal: "2.5%",
     height: "100%",
     width: "100%"
   },
-  circle: {
-    marginTop: "10%",
-    marginBottom: "10%" ,
-    width: "20%",
-    height: "10%",
-    borderRadius: "50%",
-    borderWidth: 2,
-    borderColor: "#83C3FF",
+  postText: {
+    fontSize: 20,
+    weight: 'bold'
+  },
+  post: {
+    width: "90%",
+    height: "7%",
+    backgroundColor: "#83C3FF",
+    borderRadius: 10,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  header: {
+    zIndex: 1,
+    position: 'absolute',
+    height: 100,
+    width: "100%",
+    paddingBottom: "3%",
+    paddingTop: "13%",
+    paddingHorizontal: "5%",
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#83C3FF',
+    overflow: 'hidden',
+  },
+  body: {
+    width: "100%",
+    height: "100%",
+    paddingTop: "10%",
+    paddingBottom: "10%",
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
+
   },
   box: {
-    paddingTop: "3%",
-    marginTop: "10%",
     width: "90%",
-    height: "6%",
+    height: "10%",
     borderRadius: "15%",
     borderWidth: 2,
     borderColor: "#83C3FF",
     alignItems: 'center',
     justifyContent: 'center',
+
   },
   Button: {
     backgroundColor: "white",
@@ -129,18 +238,19 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: "center"
   },
-  postText: {
-    fontSize: 20,
-    weight: 'bold'
-  },
-  save: {
-    marginTop: "10%",
-    width: "90%",
-    height: "6%",
-    backgroundColor: "#83C3FF",
-    borderRadius: 10,
+  container: {
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+  },
+  stars: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  starUnselected: {
+    color: '#aaa',
+  },
+  starSelected: {
+    color: '#ffb300',
   },
 });
 
