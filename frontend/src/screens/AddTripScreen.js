6147548582bc80c8045df9252a686d37f90082f5
Bob Shens <kiranmanikonda123@gmail.com>
2023-05-02 19:51:20 -0700

    update upload

--- frontend/src/screens/AddTripScreen.js
+++ frontend/src/screens/AddTripScreen.js
@@ -35,7 +35,9 @@ const AddTripScreen = ({ navigation }) => {
       quality: 1,
     });
     if (!_image.canceled) {
-      setImage(_image.assets[0]['uri']);
+      //setImage(_image.assets[0]['uri']);
+      const base64 = await FileSystem.readAsStringAsync(_image.assets[0]['uri'], { encoding: 'base64' });
+      setImage(base64);
     }
   };
 
